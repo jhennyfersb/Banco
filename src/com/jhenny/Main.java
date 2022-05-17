897e9d77a0b015f97923c4b05b0acb2ea2feef03
Jhennyfer Sobrinho <jhennyffersobrinho@gmail.com>
2022-05-17 14:07:41 -0300

    Initial commit

---
+++ src/com/jhenny/Main.java
@@ -0,0 +1,20 @@
+package com.jhenny;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente jhennyfer = new Cliente();
+        jhennyfer.setNome("Jhennyfer");
+
+        Conta cc = new ContaCorrente(jhennyfer);
+        cc.depositar(100);
+
+        Conta poupanca = new ContaPoupanca(jhennyfer);
+        cc.transferir(100,poupanca);
+
+        cc.sacar(150);
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+}
